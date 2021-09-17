00584e3e6dafd5f87b06d0c887ca1f9ecf36dd02
skullr <50787557+skullr2518@users.noreply.github.com>
2021-09-17 09:58:42 +0300

    laravel instalation and layout setup

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- sidebar -->
+        @include('layouts.sidebar')
+        <!-- end sidebar-->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+
+                <!-- topbar -->
+                @include('layouts.topbar')
+                <!-- topsidebar-->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            @include('layouts.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+@endsection
